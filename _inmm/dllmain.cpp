3faae893586547cbab74cbf22b02d7dc6f67f774
hhyy_best <hhyy_best@hotmail.com>
2017-07-06 00:26:11 +0800

    dll版破解

--- _inmm/dllmain.cpp
+++ _inmm/dllmain.cpp
@@ -1,6 +1,6 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
-
+extern void ExtraPatch();
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -9,6 +9,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+        ExtraPatch(); break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
